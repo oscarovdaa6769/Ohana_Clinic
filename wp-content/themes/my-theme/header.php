e77408f7fd69d4c1065688eae6f886a0431de716
oscarovdaa6769 <oudomveasnadet67@gmail.com>
2025-12-23 08:46:58 +0700

    add git ignore

---
+++ wp-content/themes/my-theme/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/header.css">
+      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
+<body>
+      
+</body>
+</html>
