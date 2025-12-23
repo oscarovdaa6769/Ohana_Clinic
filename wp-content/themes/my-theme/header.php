6de29ff9db135070c4ac73afe7b946986af752d7
oscarovdaa6769 <oudomveasnadet67@gmail.com>
2025-12-23 16:45:36 +0700

    add navigation

--- wp-content/themes/my-theme/header.php
+++ wp-content/themes/my-theme/header.php
@@ -7,6 +7,20 @@
       <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 </head>
 <body>
-      
+      <header>
+            <div class="logo">
+                  <img src="/wp-content/public/assets/images/clinic-logo.png" alt="Ohana-clinic">
+            </div>
+            <nav>
+                  <ul>
+                        <li>Home</li>
+                        <li>First visit information</li>
+                        <li>About our hospital</li>
+                        <li>Medical Information</li>
+                        <li>inquity</li>
+                        <li>Access and Consultation Hours</li>
+                  </ul>
+            </nav>
+      </header>
 </body>
 </html>
