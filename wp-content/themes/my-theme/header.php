fb5a4e44996a7fddce0001796d15aae8c126f948
oscarovdaa6769 <oudomveasnadet67@gmail.com>
2025-12-25 09:39:23 +0700

    manage css folder

--- wp-content/themes/my-theme/header.php
+++ wp-content/themes/my-theme/header.php
@@ -3,22 +3,33 @@
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/header.css">
-      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/layout/header.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 </head>
 <body>
       <header>
             <div class="logo">
-                  <img src="/wp-content/public/assets/images/clinic-logo.png" alt="Ohana-clinic">
+                  <img src="/wp-content/themes/my-theme/assets/images/clinic-logo.png" alt="Ohana-clinic">
             </div>
             <nav>
+                  <div class="cta">
+                        <div class="phone-link">
+                              <i class="fa-solid fa-phone"></i>
+                              <p>012-345-6789</p>
+                        </div>
+                        <div class="reservation-btn">
+                              <i class="fa-solid fa-calendar-days"></i>
+                              <p>online reservation</p>
+                        </div>
+                  </div>
                   <ul>
-                        <li>Home</li>
-                        <li>First visit information</li>
-                        <li>About our hospital</li>
-                        <li>Medical Information</li>
-                        <li>inquity</li>
-                        <li>Access and Consultation Hours</li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">First visit information</a></li>
+                        <li><a href="#">About our hospital</a></li>
+                        <li><a href="#">Medical Information</a></li>
+                        <li><a href="#">inquity</a></li>
+                        <li><a href="#">Access and Consultation Hours</a></li>
                   </ul>
             </nav>
       </header>
