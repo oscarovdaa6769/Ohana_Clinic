d78c9d90e013060b0ddd817f73f9dc5b4c9ea6a9
haitheara <haitheara1111@gmail.com>
2025-12-30 15:33:04 +0700

    final commit

---
+++ wp-content/themes/my-theme/page-about-our-hospital.php
@@ -0,0 +1,71 @@
+<?php get_header () ?>
+<link rel="stylesheet" href="<?php get_template_directory_uri () ?>/style.css">
+<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/grid.css">
+<div class="inquiry_section">
+<main>      
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+      <div class="hero_section">
+             <h3> Doctor introduction</h3>
+            </div>
+            <div class="navcation">      <ul>
+                  <li><a href="">Home</a> > <a href="">About our hospital</a> > </li>
+                  <li>Doctor introduction</li>
+                </ul>
+      </div>
+      <div class="doctor_information">
+            <div class="text_information">
+              <h1>Yasunori Yoshimura</h1>
+              <div class="line_information_doctor"></div>
+              <p> <br>Keiai Clinic Ethics Committee Chairman <br> <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Cabinet Secretariat Advisor <br><i class="fa-solid fa-arrows-to-dot"></i> 
+              Professor Emeritus, Department of Obstetrics and Gynecology, Keio University School of  Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japan Society of Obstetrics and Gynecology <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japanese Society of Reproductive Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Chairman of the Japan Society of Gynecological and Obstetric Endoscopy <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Vice President of Fukushima Medical University</p>
+            </div>
+            <div class="img_doctor"> <img src="https://i.pinimg.com/1200x/77/68/01/776801aab24501e763269088d72595f2.jpg" alt=""></div>
+      </div>
+            <div class="doctor_information">
+            <div class="text_information">
+              <h1>Toranobu Okuyama</h1>
+              <div class="line_information_doctor"></div>
+              <p> <br>Keiai Clinic Ethics Committee Chairman <br> <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Cabinet Secretariat Advisor <br><i class="fa-solid fa-arrows-to-dot"></i> 
+              Professor Emeritus, Department of Obstetrics and Gynecology, Keio University School of  Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japan Society of Obstetrics and Gynecology <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japanese Society of Reproductive Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Chairman of the Japan Society of Gynecological and Obstetric Endoscopy <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Vice President of Fukushima Medical University</p>
+            </div>
+            <div class="img_doctor"> <img src="https://i.pinimg.com/736x/6c/6e/d7/6c6ed7f4011b7f926b3f1505475aba16.jpg" alt=""></div>
+      </div>
+         <div class="doctor_information">
+            <div class="text_information">
+              <h1>Mamoru Tanaka</h1>
+              <div class="line_information_doctor"></div>
+              <p> <br>Keiai Clinic Ethics Committee Chairman <br> <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Cabinet Secretariat Advisor <br><i class="fa-solid fa-arrows-to-dot"></i> 
+              Professor Emeritus, Department of Obstetrics and Gynecology, Keio University School of  Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japan Society of Obstetrics and Gynecology <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japanese Society of Reproductive Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Chairman of the Japan Society of Gynecological and Obstetric Endoscopy <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Vice President of Fukushima Medical University</p>
+            </div>
+            <div class="img_doctor"> <img src="https://i.pinimg.com/1200x/f3/d6/e0/f3d6e02b9ceafd5347fc58aa1341b6fe.jpg" alt=""></div>
+      </div>
+         <div class="doctor_information">
+            <div class="text_information">
+              <h1>Masakazu Ohashi</h1>
+              <div class="line_information_doctor"></div>
+              <p> <br>Keiai Clinic Ethics Committee Chairman <br> <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Cabinet Secretariat Advisor <br><i class="fa-solid fa-arrows-to-dot"></i> 
+              Professor Emeritus, Department of Obstetrics and Gynecology, Keio University School of  Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japan Society of Obstetrics and Gynecology <br>
+              <i class="fa-solid fa-arrows-to-dot"></i>Former Chairman of the Japanese Society of Reproductive Medicine <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Chairman of the Japan Society of Gynecological and Obstetric Endoscopy <br>
+              <i class="fa-solid fa-arrows-to-dot"></i> Former Vice President of Fukushima Medical University</p>
+            </div>
+            <div class="img_doctor"> <img src="https://i.pinimg.com/736x/3c/aa/34/3caa34c3168f7e902fd1e060d7f11446.jpg" alt=""></div>
+      </div>
+</main>
