c3d5429b2d49a880f47ca72d18d44c66d448d1f3
haitheara <haitheara1111@gmail.com>
2026-01-02 09:01:42 +0700

    .

--- wp-content/themes/my-theme/page-introduction-to-the-hospital.php
+++ wp-content/themes/my-theme/page-introduction-to-the-hospital.php
@@ -7,7 +7,7 @@
 
 
     <div class="hero_section">
-             <h3> Introduction to the hospital</h3>
+             <h3> Clinic exterior</h3>
             </div>
             <div class="navcation">      <ul>
                   <li><a href="">Home</a> > <a href="">About our hospital</a> > </li>
@@ -15,17 +15,54 @@
                 </ul>
       </div>
 
-     <div class="section_clinic-exterior">
-        <h1>Clinic exterior</h1>
-       <div class="clinic-exterior_img">
-         <img src="https://i.pinimg.com/1200x/32/fb/57/32fb57603ab8a58e50a29b3e6fc5e42c.jpg" alt="">
-       </div>
-       <div class="clinic-exterior_img">
-         <img src="https://i.pinimg.com/1200x/32/fb/57/32fb57603ab8a58e50a29b3e6fc5e42c.jpg" alt="">
-       </div>
-     </div>
+   <section class="culture-room">
+    <h2>Clinic exterior</h2>
+    <div class="underline"></div>
 
+    <div class="culture-gallery">
+        <div class="img-box">
+            <img src="https://i.pinimg.com/1200x/32/fb/57/32fb57603ab8a58e50a29b3e6fc5e42c.jpg" alt="Culture Room Image 1">
+        </div>
+        <div class="img-box">
+            <img src="https://i.pinimg.com/1200x/e9/dc/0f/e9dc0fede522eb33d9b52dc678d36363.jpg" alt="Culture Room Image 2">
+        </div>
+    </div>
+</section>
+<section class="culture-room">
+    <h2>Waiting Roam</h2>
+    <div class="underline"></div>
 
+    <div class="culture-gallery">
+        <div class="img-box">
+            <img src="https://i.pinimg.com/1200x/d8/ba/0b/d8ba0b2762bd6901880b367a7bb3acce.jpg" alt="Culture Room Image 1">
+        </div>
+        <div class="img-box">
+            <img src="https://i.pinimg.com/736x/36/c5/28/36c5286f8f150bf662214022935332c4.jpg" alt="Culture Room Image 2">
+        </div>
+         <div class="img-box">
+            <img src="https://i.pinimg.com/736x/36/c5/28/36c5286f8f150bf662214022935332c4.jpg" alt="Culture Room Image 2">
+        </div>
+    </div>
+</section>
+
+<section class="culture-room">
+      <h2>Blood collection and treatment room</h2>
+    <div class="underline"></div>
+
+    <div class="culture-gallery">
+        <div class="img-box">
+            <img src="https://i.pinimg.com/1200x/f8/61/08/f86108dc500459481b4e79c19e12a88c.jpg" alt="Culture Room Image 1">
+        </div>
+</section>
+<section class="culture-room">
+      <h2>Recovery Room</h2>
+    <div class="underline"></div>
+
+    <div class="culture-gallery">
+        <div class="img-box">
+            <img src="https://i.pinimg.com/1200x/11/25/99/112599fb0116bb7cee72594d266aa367.jpg" alt="Culture Room Image 1">
+        </div>
+</section>
 
 
 
